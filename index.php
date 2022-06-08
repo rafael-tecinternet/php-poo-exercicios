6ca2785b8120c50620a29163cdc8b569f186cd1b
rafael-tecinternet <rafah1194@gmail.com>
2022-06-08 11:38:23 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício PHP - POO</title>
+    <title>Exercício PHP - POO</title> 
 </head>
 <body>
     <h1>Exercício 1</h1>
@@ -21,12 +21,14 @@ $livro2->setTitulo("Rua sem saida");
 $livro2->setAutor("M.Neto Filho");
 $livro2->setPaginas(350);
 ?>
-<h3><?=$livro->getTitulo()?></h3>
-<h3><?=$livro->getAutor()?></h3>
-<h3><?=$livro->getPaginas()?></h3>
-<hr>
-<h3><?=$livro2->getTitulo()?></h3>
-<h3><?=$livro2->getAutor()?></h3>
-<h3><?=$livro2->getPaginas()?></h3>
+<h2>Dados:</h2>
+    <h3><?=$livro->getTitulo()?></h3>
+    <p><?=$livro->getAutor()?></p>
+    <p><?=$livro->getPaginas()?></p>
+
+    <h3><?=$livro2->getTitulo()?></h3>
+    <p><?=$livro2->getAutor()?></p>
+    <p><?=$livro2->getPaginas()?></p>
+
 </body>
 </html>
